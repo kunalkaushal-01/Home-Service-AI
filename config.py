90d03d19d8fda15a2a70d1e740bacbc03c57e52f
Kunal Kaushal <kunal1085@gmail.com>
2025-09-22 12:14:30 +0530

    Updte config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
@@ -12,8 +12,17 @@ DB_URL = os.getenv("DB_URL")
 if not DB_URL:
     raise ValueError("❌ DB_URL is not set in the .env file")
 
-# SQLAlchemy engine
-engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
+try:
+    # SQLAlchemy engine
+    engine = create_engine(DB_URL, pool_pre_ping=True, future=True)
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT version();"))
+    print("✅ Database connection successful")
+except Exception as e:
+    import traceback
+    print("❌ Connection failed:")
+    traceback.print_exc()
+    
 
 # SessionLocal for DB operations
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
